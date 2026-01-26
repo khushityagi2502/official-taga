8d3fa3b9209dc88dc23b37aee40cded126290c10
khushityagi2502 <tyagikhushijp@gmail.com>
2026-01-26 17:00:35 +0530

    Add signup and login functions to script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function sayHi(){
-  alert("Hello!website ka button kaam kar raha hai");
+function signup() {
+  let user = document.getElementById("suUser").value;
+  let pass = document.getElementById("suPass").value;
+
+  if (user === "" || pass === "") {
+    document.getElementById("msg").innerText = "Fill all fields!";
+    return;
+  }
+
+  localStorage.setItem("username", user);
+  localStorage.setItem("password", pass);
+
+  document.getElementById("msg").innerText = "Signup successful ✅";
+}
+
+function login() {
+  let user = document.getElementById("liUser").value;
+  let pass = document.getElementById("liPass").value;
+
+  let savedUser = localStorage.getItem("username");
+  let savedPass = localStorage.getItem("password");
+
+  if (user === savedUser && pass === savedPass) {
+    document.getElementById("msg").innerText = "Login successful 🎉";
+  } else {
+    document.getElementById("msg").innerText = "Wrong username or password ❌";
+  }
 }
